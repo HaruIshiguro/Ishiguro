78c2b76ef05fdc19980f98177a05a835ffa316bc
HaruIshiguro <251205014@ccmailg.meijo-u.ac.jp>
2026-05-25 15:19:40 +0900

    青色にする

--- canvas-tetris-main/js/tetris.js
+++ canvas-tetris-main/js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'skyblue', 'orange', 'blue', 'yellow', 'red', 'green', 'gray'
+    'skyblue', 'orange', 'blue', 'yellow', 'red', 'blue', 'gray'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
